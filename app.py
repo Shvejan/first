3bf468f1d42da3ed36bc9a512895abefd6c85a07
Shvejan <shvejan2011@gmail.com>
2019-09-24 22:05:34 +0530

    new yr check

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask
+import datetime
+from flask import Flask,render_template\
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "hola wrld!"
 
-@app.route("/<string:name>")
-def next(name):
-    #name = name.capitalize()
-    return f"<h1>hola ,{name}!<h1>"
-@app.route("/hola/mexi")
-def sec():
-    #name = name.capitalize()
-    return f"<h1>hola hollaoaa!<h1>"
+@app.route("/ht")
+def next():
+    head = "hezup"
+    return render_template("index.html", head = head )
+
+
+@app.route("/isitnewyr")
+def chk():
+    now = datetime.datetime.now()
+    yr = now.month==1 and now.date==1
+    if yr:
+        return render_template("chk.html" , bool = "yo bro")
+    else:
+        return render_template("chk.html" , bool = "nope yaar")
