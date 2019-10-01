385cf5a0aec5774fad7c4ca8b543ea550eb7b86d
Shvejan <shvejan2011@gmail.com>
2019-10-01 20:49:15 +0530

    all set for the game

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import datetime
+import random
 from flask import Flask,render_template\
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "hola wrld!"
+def home():
+    return render_template("home.html" )
 
-@app.route("/ht")
+@app.route("/index")
 def next():
     head = "hezup"
     return render_template("index.html", head = head )
@@ -18,3 +19,12 @@ def chk():
     now = datetime.datetime.now()
     yr = now.month==1 and now.date==1
     return render_template("chk.html", yr=yr)
+
+
+@app.route("/link")
+def link():
+    return render_template("link.html")
+
+@app.route("/game")
+def game():
+    return render_template("game.html" )
