188da9e21f189fb601f2894f2c8383235a5479ab
Shvejan <shvejan2011@gmail.com>
2019-10-04 15:42:47 +0530

    database linked!

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-import datetime
-import random
-from flask import Flask,render_template,request,session
+import os
+import psycopg2
+from flask import Flask, session,render_template
 from flask_session import Session
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+
+conn = psycopg2.connect(
+    database="test",
+    user="postgres",
+    password = "@shivatejan1",
+    host = "localhost",
+    port="5432"
+)
+cur=conn.cursor()
 
 
 @app.route("/")
@@ -51,12 +62,17 @@ def res():
 def ses():
     if session.get("notes") == None:
         session["notes"] = []
-    if request.method == "POST":
-        session["notes"] = []
-
 
     if request.method == "POST":
         note = request.form.get("n")
         session["notes"].append(note)
 
     return render_template("ses.html", notes=session["notes"])
+
+
+@app.route("/db")
+def db():
+
+    cur.execute("SELECT * FROM flights WHERE duration < 350 ")
+    rows = cur.fetchall()
+    return render_template("db.html" , rows = rows)
