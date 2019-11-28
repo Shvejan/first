8f22b2c7dbe96275808eb9da0fd2f4b8127de7a7
Shvejan <shvejan2011@gmail.com>
2019-11-28 12:22:09 +0530

    js tasks done

---
+++ js/cnt3.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function(){
+  document.querySelector("button").onclick=cnt;
+}
+);
+let  c=0;
+
+function cnt()
+{
+  c++;
+  document.querySelector('h1').innerHTML=c;
+  document.querySelector('button').innerHTML=hehehe;
+
+}
